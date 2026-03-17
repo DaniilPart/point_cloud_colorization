d1f48c0760d909060b13bd70164eab8e33fb04e9
DaniilPart <Z3nin>
2026-03-17 21:08:03 +0100

    Purple points/task 3

--- src/my_node.cpp
+++ src/my_node.cpp
@@ -1,8 +1,12 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
 using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node
@@ -13,15 +17,44 @@ class MinimalSubscriber : public rclcpp::Node
     {
       subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
       "/os1/points", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    }
 
+      // make publisher for new data
+      publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/colorizer/debug/purple_cloud", 10);
+    }
     
   private:
     void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard a point cloud with %d points", msg->width * msg->height);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
+      pcl::fromROSMsg(*msg, *cloud_in);
+
+      pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
+
+      for (size_t i = 0; i < cloud_in->points.size(); ++i) {
+          pcl::PointXYZRGB color_point;
+          color_point.x = cloud_in->points[i].x;
+          color_point.y = cloud_in->points[i].y;
+          color_point.z = cloud_in->points[i].z;
+          
+          color_point.r = 128;
+          color_point.g = 0;
+          color_point.b = 128;
+
+          cloud_out->points.push_back(color_point);
+      }
+
+      sensor_msgs::msg::PointCloud2 msg_out;
+      pcl::toROSMsg(*cloud_out, msg_out);
+      msg_out.header = msg->header;
+
+      publisher_->publish(msg_out);
+
+      RCLCPP_INFO(this->get_logger(), "Pointcloud received with %d points", msg->width * msg->height);
     }
+    
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+
 };
 
 int main(int argc, char * argv[])

55b2267be1d6d0438b078e0c504a509d7028b7e3
DaniilPart <Z3nin>
2026-03-18 19:01:15 +0100

    Time Synchronization

--- src/my_node.cpp
+++ src/my_node.cpp
@@ -8,65 +8,43 @@
 #include <pcl/point_types.h>
 #include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
 
 using std::placeholders::_1;
+using std::placeholders::_2;
 
 class MinimalSubscriber : public rclcpp::Node
 {
+    typedef message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::msg::PointCloud2,
+      sensor_msgs::msg::CompressedImage
+    > MySyncPolicy;
+
+    typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+
   public:
     MinimalSubscriber()
     : Node("minimal_subscriber")
     {
-      subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-      "/os1/points", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      lidar.subscribe(this, "/os1/points" );
+      camera.subscribe(this, "/camera_front/image_raw/compressed" );
+      sync_.reset(new Sync(MySyncPolicy(10), lidar, camera));
+      sync_->registerCallback(std::bind(&MinimalSubscriber::topic_callback, this, _1, _2));
 
-      subscription1_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
-      "/camera_front/image_raw/compressed", 10, std::bind(&MinimalSubscriber::image_callback, this, _1));
-
-      publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/colorizer/debug/purple_cloud", 10);
     }
-    
   private:
-    void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) const
+    void topic_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg, const sensor_msgs::msg::CompressedImage::ConstSharedPtr img_msg) const
     {
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::fromROSMsg(*msg, *cloud_in);
-
-      pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZRGB>);
-
-      for (size_t i = 0; i < cloud_in->points.size(); ++i) {
-          pcl::PointXYZRGB color_point;
-          color_point.x = cloud_in->points[i].x;
-          color_point.y = cloud_in->points[i].y;
-          color_point.z = cloud_in->points[i].z;
-          
-          color_point.r = 128;
-          color_point.g = 0;
-          color_point.b = 128;
-
-          cloud_out->points.push_back(color_point);
-      }
-
-      sensor_msgs::msg::PointCloud2 msg_out;
-      pcl::toROSMsg(*cloud_out, msg_out);
-      msg_out.header = msg->header;
-
-      publisher_->publish(msg_out);
+      RCLCPP_INFO(this->get_logger(), "Synchronized pair received at time %d.%d and %d.%d", 
+                  msg->header.stamp.sec, msg->header.stamp.nanosec, img_msg->header.stamp.sec, img_msg->header.stamp.nanosec);
     }
 
-    void image_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg) const
-    {
-      cv::Mat cv_image = cv_bridge::toCvCopy(msg, "bgr8")->image;
-      int w = cv_image.cols;
-      int h = cv_image.rows; 
-      int size = h * w;
-      RCLCPP_INFO(this->get_logger(), "Image received with shape: %d x %d", w, h);
-    }
-    
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+    message_filters::Subscriber<sensor_msgs::msg::PointCloud2> lidar;
+    message_filters::Subscriber<sensor_msgs::msg::CompressedImage> camera;
+    std::shared_ptr<Sync> sync_;
 
-    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr subscription1_;
 
 };
 

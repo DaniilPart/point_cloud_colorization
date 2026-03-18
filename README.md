ca9d0caa09778577011fae820f94ea42cf1c94c9
DaniilPart <Z3nin>
2026-03-18 15:41:40 +0100

    Image Ingestion/ task4

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 1. Initial git project, create empty node 17.03
+2. Point Cloud Ingestion 17.03
+3. Basic Colorization and Visualization (Purple Cloud) 17.03
+4. Image Ingestion 18.03
--- src/my_node.cpp
+++ src/my_node.cpp
@@ -2,10 +2,12 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
-
+#include "sensor_msgs/msg/compressed_image.hpp"
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <cv_bridge/cv_bridge.hpp>
+#include <opencv2/opencv.hpp>
 
 using std::placeholders::_1;
 
@@ -18,7 +20,9 @@ class MinimalSubscriber : public rclcpp::Node
       subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
       "/os1/points", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
 
-      // make publisher for new data
+      subscription1_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
+      "/camera_front/image_raw/compressed", 10, std::bind(&MinimalSubscriber::image_callback, this, _1));
+
       publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/colorizer/debug/purple_cloud", 10);
     }
     
@@ -48,12 +52,21 @@ class MinimalSubscriber : public rclcpp::Node
       msg_out.header = msg->header;
 
       publisher_->publish(msg_out);
+    }
 
-      RCLCPP_INFO(this->get_logger(), "Pointcloud received with %d points", msg->width * msg->height);
+    void image_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg) const
+    {
+      cv::Mat cv_image = cv_bridge::toCvCopy(msg, "bgr8")->image;
+      int w = cv_image.cols;
+      int h = cv_image.rows; 
+      int size = h * w;
+      RCLCPP_INFO(this->get_logger(), "Image received with shape: %d x %d", w, h);
     }
     
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+
+    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr subscription1_;
 
 };
 
